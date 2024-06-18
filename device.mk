de25da3d0011f4d8a40a4f7c4a42f3df1f0ac1da
Ravinderrohilla <ravinderrohilla.002@gmail.com>
2024-06-18 10:48:31 +0530

    peridot: Initial bringup for decryption

--- device.mk
+++ device.mk
@@ -10,8 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Configure core_64_bit_only.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
-# Configure Virtual A/B
+# Virtual A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# A/B updater updatable partitions list. Keep in sync with the partition list
+# with "_a" and "_b" variants in the device. Note that the vendor can add more
+# more partitions to this list for the bootloader and radio.
+AB_OTA_PARTITIONS ?= boot vendor_boot recovery vendor_dlkm dtbo vbmeta super init_boot
 
 # Configure virtual_ab compression.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
@@ -22,9 +27,30 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Configure twrp common.mk
 $(call inherit-product, vendor/twrp/config/common.mk)
 
+# Build bootctl
 PRODUCT_PACKAGES += \
-    bootctrl.xiaomi_sm8550.recovery \
-    android.hardware.boot@1.2-impl-qti.recovery
+        bootctl
+RECOVERY_BINARY_SOURCE_FILES += $(TARGET_OUT_EXECUTABLES)/bootctl
+
+PRODUCT_PACKAGES +=\
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
+    
+# Update engine 
+PRODUCT_PACKAGES +=\
+    update_engine \
+    update_engine_client \
+    update_verifier \
+    update_engine_sideload 
+
+ # Platform
+QCOM_BOARD_PLATFORMS += $(PRODUCT_PLATFORM)
+TARGET_BOARD_PLATFORM := $(PRODUCT_PLATFORM)
+TARGET_BOOTLOADER_BOARD_NAME := $(TARGET_BOARD_PLATFORM)
+  
+# A/B partitioned
+AB_OTA_UPDATER := true
 
 # SHIPPING API
 PRODUCT_SHIPPING_API_LEVEL := 31
@@ -32,11 +58,24 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 # VNDK API
 PRODUCT_TARGET_VNDK_VERSION := 34
 
+# qcom decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+        
+# fastbootd
+PRODUCT_PACKAGES += \
+     fastbootd
+
+# Add default implementation of fastboot HAL.
+PRODUCT_PACKAGES += android.hardware.fastboot@1.1-impl-mock
+
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Soong namespaces
-PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
+PRODUCT_SOONG_NAMESPACES += \
+      $(DEVICE_PATH)
 
 # OEM otacerts
 PRODUCT_EXTRA_RECOVERY_KEYS += \
